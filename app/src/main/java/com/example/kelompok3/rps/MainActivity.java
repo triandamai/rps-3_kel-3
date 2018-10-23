9e0345b9fe3409e113c03672df60a5a82df346a2
unknown <triannurizkillah@gmail.com>
2018-10-22 19:09:55 -0700

    rps 3_xml 1.1

--- app/src/main/java/com/example/kelompok3/rps/MainActivity.java
+++ app/src/main/java/com/example/kelompok3/rps/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.kelompok3.rps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import org.w3c.dom.Document;
@@ -10,18 +14,23 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv1;
+    ListView ls ;
+
+  ArrayList<HashMap<String, String>> userList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv1=(TextView)findViewById(R.id.textView1);
-
+        ls = findViewById(R.id.ls);
         try {
             InputStream is = getAssets().open("file.xml");
 
@@ -39,11 +48,19 @@ public class MainActivity extends AppCompatActivity {
                 Node node = nList.item(i);
                 if (node.getNodeType() == Node.ELEMENT_NODE) {
                     Element element2 = (Element) node;
-                    tv1.setText(tv1.getText()+"\nName : " + getValue("name", element2)+"\n");
-                    tv1.setText(tv1.getText()+"Surname : " + getValue("surname", element2)+"\n");
-                    tv1.setText(tv1.getText()+"-----------------------");
+                    HashMap<String,String> user = new HashMap<>();
+//                    tv1.setText(tv1.getText()+"\nName : " + getValue("name", element2)+"\n");
+//                    tv1.setText(tv1.getText()+"Surname : " + getValue("surname", element2)+"\n");
+//                    tv1.setText(tv1.getText()+"-----------------------");
+                    user.put("name",getValue("name",element2));
+                    user.put("surname",getValue("surname",element2));
+                    userList.add(user);
+
+
                 }
             }
+            ListAdapter adapter = new SimpleAdapter(MainActivity.this,userList,R.layout.item,new String[]{"name","surname"}, new int[]{R.id.tv_list,R.id.tv_list2});
+            ls.setAdapter(adapter);
 
         } catch (Exception e) {e.printStackTrace();}
 
